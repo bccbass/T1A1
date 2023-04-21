ee04f8ee8afb94d4fc22a28f195e9d4d9fc002bb
bccbass <109984363+bccbass@users.noreply.github.com>
2023-04-21 14:16:34 +1000

    init commit

---
+++ q15/weather_summary.py
@@ -0,0 +1,33 @@
+def weatherSummaryOne(isRaining, temp):
+    if isRaining:
+        if temp < 15:
+            print("It's wet and cold")
+        else:
+            print("It's warm and raining")
+    else:
+        if temp < 15:
+            print("It's not raining but cold")
+        else:
+            print("It's warm but not raining")
+
+
+
+def weatherSummaryTwo(isRaining, temp):
+    if isRaining and temp < 15:
+        print("It's wet and cold")
+    elif isRaining and temp >= 15:
+        print("It's warm and raining")
+    elif not isRaining and temp < 15:
+        print("It's not raining but cold")
+    else:
+        print("It's warm but not raining")
+
+
+# weatherSummary(True, 14)
+
+celsius = float(input())
+
+farehnheit = (celsius*9/5)+32
+
+print(f"The result is: {farehnheit}.")
+
