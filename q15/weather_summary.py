dcce308a74d15cd570fa08007d7543a5895c9d19
bccbass <109984363+bccbass@users.noreply.github.com>
2023-04-22 18:33:17 +1000

    q13

--- q15/weather_summary.py
+++ q15/weather_summary.py
@@ -25,9 +25,25 @@ def weatherSummaryTwo(isRaining, temp):
 
 # weatherSummary(True, 14)
 
-celsius = float(input())
+# celsius = float(input())
 
-farehnheit = (celsius*9/5)+32
+# farehnheit = (celsius*9/5)+32
 
-print(f"The result is: {farehnheit}.")
+# print(f"The result is: {farehnheit}.")
+
+arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+
+i = 0
+
+while (i < len(arr) -1) and (arr[i] < arr[i+1]):
+    i += 1
+print(i)
+indexCache = arr[i]
+arr[i] = arr[i+1]
+arr[i+1] = indexCache
+
+
+
+print(arr)
+
 
