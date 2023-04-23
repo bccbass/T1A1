8bf1d2272c3979f4dc93ed5db35134532ee6e76f
bccbass <109984363+bccbass@users.noreply.github.com>
2023-04-23 22:52:01 +1000

    q16 solve

---
+++ q16/q16.py
@@ -0,0 +1,42 @@
+skills = {
+    'Python' : 1,
+    'Ruby' : 2,
+    'Bash' : 4,
+    'Git' : 8,
+    'HTML' : 16,
+    'TDD' : 32,
+    'CSS' : 64,
+    'JavaScript' : 128,
+}
+
+applicant_skills = []
+skills_to_learn = []
+applicant_score = 0
+
+print('Enter programming languages known. Press enter when complete.')
+
+
+while True:
+    skill = input()
+    if skill == '':
+        break
+    elif skill not in skills:
+        print("I don't know that language, please check and try again.")
+    else:
+        applicant_skills.append(skill)
+ 
+
+for skill in skills:
+    if skill in applicant_skills:
+        applicant_score += skills[skill]
+    else:
+        skills_to_learn.append(skill)
+        
+print(f'Your overall coding skill score is {applicant_score}')
+
+for skill in skills_to_learn:
+    print(f'Consider learning {skill}, it could boost your score by {skills[skill]}.')
+
+print('Thank you for your interest in ACME Corporation!')
+
+    
